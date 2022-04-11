ce8ae83270fa741aac3faaf8c31dbb65bcf1a371
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-04-11 15:12:49 -0300

    Reorganização e clique

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
+//Variáveis globais
 
-//distribuição das cartas
-// - o usuário será perguntado com quantas cartas quer jogar(utilizar prompt)
-//verificar utilizar uma página inicial com overlay escondido 
-
-//- o usuário deverá escolher de 4 a 14, números pares. Para qualquer número qu
-//fuja da regra, o prompt deverá fiar sendo repetido, até que o usuário coloque 
-// um número válido
 
 let quantidadeCartas; 
+let yfirstCard = true; 
+let firstCard; 
+let secondCard; 
+let endGame = 0; 
+let clickCounter = 0; 
 
 function pergunta() {
     quantidadeCartas = prompt(`Caro jador(a), seja bem-vindo(a) ao PARROT CARD GAME!!\n 
@@ -20,7 +19,6 @@ Por exemplo, não é possível jogar escolhendo 2 cartas, pois apesar de 2 ser u
 
 }
  
-
 //função para verificar se o número digitado está entre 4 e 14 e é par
 function verificaValor() {
     pergunta(); 
@@ -30,15 +28,14 @@ function verificaValor() {
 
     //primeiro posso ver se é par
     if((cartaPar) && (cartaInterval)) {
-
-        const mostraTela = document.querySelector(".container");
-        mostraTela.classList.remove("escondido");
+        showCards();
         
 
     } else if (cartaPar === false || cartaInterval === false) {
         alert(`Caro jogador(a), seu nº de cartas (${quantidadeCartas}) não está seguindo 
         as regras. Escolha novamente, Boa sorte!`);
         pergunta(); 
+        //quantidadeCartas = 0;  
 
         setTimeout(function() {
             verificaValor();
@@ -50,23 +47,22 @@ function verificaValor() {
 }
 verificaValor(); 
 
+//cartas gif com link com class
 let cartasGif = [
-    "bobrossparrot", 
-    "explodyparrot", 
-    "fiestaparrot", 
-    "metalparrot", 
-    "revertitparrot", 
-    "tripletsparrot", 
-    "unicornparrot"
+    "card-front-bobrossparrot", 
+    "card-front-explodyparrot", 
+    "card-front-fiestaparrot", 
+    "card-front-metalparrot", 
+    "card-front-revertitparrot", 
+    "card-front-tripletsparrot", 
+    "card-front-unicornparrot"
 ]
-
-
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-const cartasNum = [0,1, 2, 3, 4, 5, 6]; 
+const cartasNum = [0, 1, 2, 3, 4, 5, 6]; 
 
 const cartasNumSort = cartasNum.sort(comparador);
 
@@ -76,9 +72,6 @@ let sGame;
 function starGame() {
 
     sGame = []; 
-    //aqui vamos pegar quantidade de cartas e dividir por dois 
-    //para ter um array de 2 itens e depois aplicamos comparador nesse 
-    //novo array
     for (let i = 0; i < (quantidadeCartas/2); i++) {
         sGame[i] = cartasNumSort[i]; 
    
@@ -95,9 +88,20 @@ arr3.sort(comparador);
 
 //obtendo array com o nome dos gifs
 
+function showCards () {
+    const cardsGame = document.querySelector(".cards-game"); 
+    for (let i = 1; i<= quantidadeCartas; i++) {
+        cardsGame.innerHTML += `
+        <div onclick="validateClick(this)" id="cardNumber${i}" class="card card-verse">
+        </div>`
+    }
+    takePictures(); 
+}
+
+
 let nameGif
 
-function Gif() { 
+function takePictures() { 
 
     nameGif = []; 
 
@@ -107,65 +111,55 @@ function Gif() {
    }
 
 }
+ 
+function validateClick(card) {
+    if (card.classList.length !== 3) {
+        selectCard(card); 
+    }
 
-Gif(); 
+}
 
-function mostrarCarta () {
-    let novo = document.querySelector(".card .back-face.face"); 
-    novo.classList.remove("escondido")
+//criando a função selectCard
+
+function selectCard (card) {
+    let nameId = (card.id);
+    nameId = nameId.replace('cardNumber', ' '); 
+    card.classList.add(nameGif[nameId-1]);
+    upturnedYNCard(card); 
 }
 
 
-function adicionarCartas () {
-    const carta = document.querySelector(".cards-game"); 
+function upturnedYNCard (card) {
+    setTimeout(function () {
 
-    carta.innerHTML = "";
-    for (let i = 0; i < nameGif.length; i++) {
+        if (yfirstCard) {
+            clickCounter++
+            firstCard = card
+            yfirstCard = false; 
+        } else {
+            secondCard = card 
+            if (firstCard.classList[2] !== secondCard.classList[2]) {
+                secondCard.classList = "card card-verse";
+                firstCard.classList = "card card-verse"; 
+            } else {
+                endGame++
+                if(quantidadeCartas/2 === endGame) {
+                    alert(`Parabéns! Você ganhou em ${clickCounter} jogadas!`); 
 
-        carta.innerHTML += `
-          <div class="card" onclick="mostrarCarta()">
-          <div class="front-face face">
-             <img src="images/front.png" >
-          </div>
+                    const backButton = document.querySelector(".hide"); 
+                    backButton.classList.remove("hide"); 
+                    const refreshPage = document.querySelector("#refresh"); 
+                    refreshPage.addEventListener("click", function () {
+                        location.reload(); 
+                    });
 
-          <div class="back-face face">
-             <img src="images/${nameGif[i]}.gif" >
-          </div>
-  </div>
-      `;
-    } 
+                }
+            }
+            yfirstCard = true
+        }
+
+    }, 1000)
 }
 
-adicionarCartas()
-
-//const cartas = [
-    //{carta4: ["bobrossparrot.gif", "explodyparrot.gif", ]}
-//]
-
-//console.log(quantidadeCartas); 
-
-//- após inserir o número de cartas válido, o jogo deverá inserir as cartas
-//viradas para baixo na página de forma que a distribuição seja aleatória
-
-//Criando um array exemplo
-
-//const minhaArray = [3, 5, 2, 6, 4, 9]; 
 
 
-
-// Esta função pode ficar separada do código acima, onde você preferir
-//function comparador() { 
-	//return Math.random() - 0.8; 
-//}
-
-//minhaArray.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
-
-//console.log(minhaArray);
-
-// - Clique na arta 
-// 
-
-
-//setTimeout(function() {
-    //console.log("Aqui executamos algo!")
-//}, 3000)
